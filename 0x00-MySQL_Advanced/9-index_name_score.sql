d784ab3c9a1b7d06b0072792be3e49c12083107c
Twayfrancis <tawaifrancis@gmail.com>
2023-12-13 21:24:16 +0300

    SQL script that creates an index idx_name_first_score on the table names and the first letter of name and the score.
    Requirements:
    
    Import this table dump: names.sql.zip
    Only the first letter of name AND score must be indexed

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,6 @@
+-- script that creates an index idx_name_first_score on table names and the first letter of name and the score
+-- import this table dump: names.sql.zip
+-- only the first letter of name and score must be indexed.
+
+CREATE INDEX idx_name_first_score
+ON names(name(1), score);
