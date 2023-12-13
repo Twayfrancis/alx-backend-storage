7dffc400f0a2e6fe2961a9f862a240def1837bb6
Twayfrancis <tawaifrancis@gmail.com>
2023-12-13 19:39:48 +0300

    SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
    Quantity in the table items can be negative.

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- script that creates a trigger that decreases the quantity of an item after adding new order
+-- quantity in the table items can be negative
+
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+FOR EACH ROW UPDATE items
+SET
+quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
