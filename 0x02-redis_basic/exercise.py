0e381b55948a0a124c4a5c06a9ef233efdc89c9d
Twayfrancis <tawaifrancis@gmail.com>
2023-12-20 14:11:57 +0300

    Redis only allows to store string, bytes and numbers (and lists thereof). Whatever you store as single elements, it will be returned as a byte string. Hence if you store "a" as a UTF-8 string, it will be returned as b"a" when retrieved from the server.
    In this exercise we will create a get method that take a key string argument and an optional Callable argument named fn. This callable will be used to convert the data back to the desired format.
    
    Remember to conserve the original Redis.get behavior if the key does not exist.
    
    Also, implement 2 new methods: get_str and get_int that will automatically parametrize Cache.get with the correct conversion function.
    
    The following code should not raise:

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ create a cache class
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -31,3 +31,43 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Get the data associated with a key from Redis.
+
+        Args:
+            key (str): The key to get the data from.
+            fn (Callable, optional): A callable that converts the data to the desired format.
+
+        Returns:
+            Union[str, bytes, int, float]: The data associated with the key.
+        """
+        data = self._redis.get(key)
+        if fn is not None and data is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Get the data associated with a key from Redis and convert it to a str.
+
+        Args:
+            key (str): The key to get the data from.
+
+        Returns:
+            str: The data associated with the key.
+        """
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Get the data associated with a key from Redis and convert it to an int.
+
+        Args:
+            key (str): The key to get the data from.
+
+        Returns:
+            int: The data associated with the key.
+        """
+        return self.get(key, fn=int)
